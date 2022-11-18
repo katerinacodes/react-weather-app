991e61c9b5142665730a7d15524057b644cba659
katerinacodes <99835475+katerinacodes@users.noreply.github.com>
2022-11-18 11:12:51 +0200

    Quick Weather app transition on VS Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from "react";
+  import axios from "axios";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [city, setCity] = useState("");
+  
+    let [text, setText] = useState("");
+    function showTemperature(response) {
+      let elements = (
+        <ul>
+          <li>Temperature : {Math.round(response.data.main.temp)}</li>
+          <li>Description : {response.data.weather[0].description}</li>
+          <li>Wind : {Math.round(response.data.wind.speed)} km/h</li>
+          <li>Humidity: {Math.round(response.data.main.humidity)}%</li>
+        </ul>
+      );
+      setText(elements);
+    }
+    function handleSubmit(event) {
+      let apiKey = "094780c710fa4efd669f0df8c3991927";
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
+      axios.get(apiUrl).then(showTemperature);
+      event.preventDefault();
+    }
+  
+    function updateCity(event) {
+      setCity(event.target.value);
+    }
+  
+    return (
+      <div>
+        <div className="div">
+          <div>Weather App</div>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Type a City" onChange={updateCity} />
+            <input className="search" type="submit" value="Search" />
+          </form>
+        </div>
+        <span className="text">{text}</span>
+      </div>
+    );
+  }
 
 export default App;
